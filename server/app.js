a2edd71fb53c692209213bbfe4f3a83ed7bf23b3
gurleen240998@gmail.com <your_gitlab_email@example.com>
2025-10-30 10:39:15 +0800

    Added app.listen for Elastic Beanstalk

--- server/app.js
+++ server/app.js
@@ -17,4 +17,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-export default app;
+const port = process.env.PORT || 8081;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
